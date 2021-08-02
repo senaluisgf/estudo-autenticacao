e09b222689cce0b005382a36e6c4ccb69be23553
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-08-02 17:55:47 -0400

    refresh: add middleware to gat refresh token and generete a new refresh token

--- src/usuarios/middlewares-autenticacao.js
+++ src/usuarios/middlewares-autenticacao.js
@@ -1,4 +1,22 @@
 const passport = require("passport")
+const { InvalidArgumentError } = require("../erros")
+const Usuario = require('../usuarios/usuarios-modelo')
+const allowlist = require('../../redis/allowlist-refresh-token')
+
+async function verificaRefreshToken(refreshToken) {
+    if(!refreshToken){
+        throw new InvalidArgumentError("nao passou o refresh token")
+    }
+    const id = await allowlist.buscaValor(refreshToken)
+    if(!id){
+        throw new InvalidArgumentError('Refresh Token invalido')
+    }
+    return id
+}
+
+async function invalidaRefreshToken(refreshToken){
+    await allowlist.deletaValor(refreshToken)
+}
 
 module.exports ={
     local: (req, res, next) => {
@@ -49,5 +67,20 @@ module.exports ={
                 return next()
             }
         )(req,res,next)
+    },
+    refresh: async (req, res, next) => {
+        try{
+            const { refreshToken } = req.body
+            const id = await verificaRefreshToken(refreshToken)
+            await invalidaRefreshToken(refreshToken)
+            req.user = await Usuario.buscaPorId(id)
+            return next()
+        } catch(erro){
+            if(error.name === "InvalidArgumentError"){
+                return res.status(401).json({erro: erro.message})
+            }
+
+            return res.status(500).json({erro: erro.message})
+        }
     }
 }
