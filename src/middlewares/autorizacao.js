277d5c0cd32bb25df3efbae16942225611fed11a
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-09-29 18:11:46 -0400

    autorizacao: verify if user can access the endpoint

--- src/middlewares/autorizacao.js
+++ src/middlewares/autorizacao.js
@@ -1,8 +1,39 @@
-module.exports = (cargosObrigatorios) => (requisicao, resposta, proximo) => {
+const controle = require('../controleDeAcesso')
+
+const metodos = {
+    ler: {
+        todos: 'readAny',
+        apenasSeu: 'readOwn'
+    },
+    criar: {
+        todos: 'createAny',
+        apenasSeu: 'createOwn'
+    },
+    remover: {
+        todos: 'deleteAny',
+        apenasSeu: 'deleteOwn'
+    }
+}
+
+module.exports = (entidade, acao) => (requisicao, resposta, proximo) => {
+    const acoes = metodos[acao]
+    const cargo = requisicao.user.cargo
+    const permissoesTodos = controle.can(cargo)[acoes.todos](entidade)
+    const permissoesApenasSeu = controle.can(cargo)[acoes.apenasSeu](entidade)
+
+    if(!permissoesTodos.granted && !permissoesApenasSeu.granted){
+        resposta.status(403).json({mensagem:'Acesso Negado'}).end()
+    }
     
-    if(cargosObrigatorios.indexOf(requisicao.user.cargo) === -1){
-        console.log("rota bloqueada")
-        resposta.status(403).json({mensagem:'Acesso Negado'})
+    requisicao.acesso = {
+        todos: {
+            permitido: permissoesTodos.granted,
+            atributos: permissoesTodos.attributes
+        },
+        apenasSeu: {
+            permitido: permissoesApenasSeu.granted,
+            atributos: permissoesApenasSeu.attributes
+        }
     }
 
     proximo()
