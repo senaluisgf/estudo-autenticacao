6998c7e99666a8ded189cc0d21175269ee493349
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-09-29 18:11:00 -0400

    autorização: add roles to system users

---
+++ src/controleDeAcesso.js
@@ -0,0 +1,20 @@
+const AccessControl = require('accesscontrol')
+const controle = new AccessControl()
+
+controle.grant('assinante')
+    .readAny('post', ['titulo', 'autor'])
+
+controle.grant('editor')
+    .extend('assinante')
+    .createOwn('post')
+    .deleteOwn('post')
+
+controle.grant('admin')
+    .readAny('post')
+    .createAny('post')
+    .deleteAny('post')
+    .readAny('usuario')
+    .createAny('usuario')
+    .deleteAny('usuario')
+
+module.exports = controle
