862c03122f0132fc9a810007393088672873c83a
Luis Guilherme(Shadow) <sena.luisgf@gmail.com>
2021-10-04 14:06:52 -0400

    emails: add a password redefinition email

--- src/usuarios/emails.js
+++ src/usuarios/emails.js
@@ -49,8 +49,19 @@ class EmailVerificacao extends Email{
     }
 }
 
+class EmailRedefinicao extends Email{
+    constructor(usuario){
+        super()
+        this.from = '"Blog do Cógido" <noreply@blogdocodigo.com.br>'
+        this.to = usuario.email
+        this.subject = 'Redefinição de senha'
+        this.text = `Olá! você solicitou redefinição de senha`
+        this.html = `<h1>Olá!</h1> você solicitou redefinição de senha`
+    }
+}
 
 module.exports = {
     Email,
-    EmailVerificacao
+    EmailVerificacao,
+    EmailRedefinicao
 }
